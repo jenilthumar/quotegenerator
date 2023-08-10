45218bdcc0f2871a64d240329c24aeca762e8f76
Jenil Thummar <108112410+jenilthumar@users.noreply.github.com>
2023-08-10 09:47:20 +0530

    Initial commit

---
+++ quotegenerator/src/test/java/com/jenil/quotegenerator/QuotegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenil.quotegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotegeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
